4a2a0349eca8148e2b34aae693779f22365150fe
mr.Rhenium <mr.rhenium75614@gmail.com>
2021-10-16 20:53:59 +0530

    Singal Page Application Part-1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,35 @@
 import React from "react";
 
+const RenderCard = ({ item }) => {
+  return (
+    <div className="card">
+      <img src={item.image} alt={item.name} className="card-img" />
+      <div className="card-body">
+        <h3 className="card-title">{item.name}</h3>
+        {item.designation ? (
+          <h5 className="card-subtitle text-muted">{item.designation}</h5>
+        ) : null}
+        <p className="card-text">{item.description}</p>
+      </div>
+    </div>
+  );
+};
+
 function Home(props) {
-  return <h1>Home Component is in working stage</h1>;
+  return (
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import Dishdetail from "./DishdetailedComponent";
-import { DISHES } from "../shared/dishes";
+// import Dishdetail from "./DishdetailedComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
 import { Switch, Route, Redirect } from "react-router";
 
 class Main extends Component {
@@ -12,41 +17,41 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      commetns: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDish: null,
     };
   }
+
   onDishSelect = (dishId) => {
     this.setState({ selectedDish: dishId });
   };
+
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
       <div className="container-fluid border border-danger w-100 h-auto">
         <Header />
         <Switch>
-          <Route path="/home" component={HomePage} />
+          <Route exact path="/home" component={HomePage} />
+          <Route exact path="/contactus" component={Contact} />
           <Route
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+
           <Redirect to="/home" />
         </Switch>
-        {/* <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => {
-            this.onDishSelect(dishId);
-          }}
-        />
-        <Dishdetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />*/}
         <Footer />
       </div>
     );

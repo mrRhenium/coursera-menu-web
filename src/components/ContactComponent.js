098db998c8d3629cfe95d4854b20979bc249ca05
mr.Rhenium <mr.rhenium75614@gmail.com>
2021-10-23 08:43:41 +0530

    Controlled Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -10,11 +10,18 @@ class Contact extends Component {
       telnum: "",
       email: "",
       agree: false,
-      contactType: "",
+      contactType: "tel",
       message: "",
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false,
+      },
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleInputChange(event) {
@@ -35,7 +42,86 @@ class Contact extends Component {
     event.preventDefault();
   }
 
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: {
+        ...this.state.touched,
+        [field]: true,
+      },
+    });
+  };
+
+  validate(firstname, lastname, telnum, email) {
+    const errors = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+    };
+
+    if (this.state.touched.firstname && firstname.length < 3) {
+      errors.firstname = "First Name should be >= 3 characters";
+    } else if (this.state.touched.firstname && firstname.length > 10) {
+      errors.firstname = "First Name should be <=10 characters";
+    }
+
+    if (this.state.touched.lastname && lastname.length < 3) {
+      errors.lastname = "Last Name should be >= 3 characters";
+    } else if (this.state.touched.lastname && lastname.length > 10) {
+      errors.lastname = "Last Name should be <=10 characters";
+    }
+
+    let reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test(telnum)) {
+      errors.telnum = "Tel. Number should only contain Number";
+    }
+
+    if (
+      this.state.touched.email &&
+      email.split("").filter((x) => x == "@").length != 1
+    ) {
+      errors.email = "Email should contain a @ ";
+    }
+
+    return errors;
+  }
+
   render() {
+    const errors = this.validate(
+      this.state.firstname,
+      this.state.lastname,
+      this.state.telnum,
+      this.state.email
+    );
+
+    // custoum function is start
+
+    let class_firstname;
+    if (this.state.touched.firstname) {
+      if (errors.firstname != "") class_firstname = "is-invalid";
+      else class_firstname = "is-valid";
+    }
+
+    let class_lastname;
+    if (this.state.touched.lastname) {
+      if (errors.lastname != "") class_lastname = "is-invalid";
+      else class_lastname = "is-valid";
+    }
+
+    let class_telnum;
+    if (this.state.touched.telnum) {
+      if (errors.telnum != "") class_telnum = "is-invalid";
+      else class_telnum = "is-valid";
+    }
+
+    let class_email;
+    if (this.state.touched.email) {
+      if (errors.email != "") class_email = "is-invalid";
+      else class_email = "is-valid";
+    }
+
+    // custom function is over
+
     return (
       <div className="container">
         <div className="row">
@@ -106,82 +192,93 @@ class Contact extends Component {
           </div>
           <div className="col-12 col-md-9">
             <form action="" onSubmit={this.handleSubmit}>
-              <div className="form-group row my-2">
+              <div className="form-group row my-4">
                 <div className="col-2">
                   <label htmlFor="firstname">First Name</label>
                 </div>
                 <div className="col-10">
                   <input
                     type="text"
-                    className="form-control"
+                    className={`form-control ${class_firstname} `}
                     name="firstname"
                     id="firstname"
                     placeholder="First Name"
                     value={this.state.firstname}
+                    onBlur={this.handleBlur("firstname")}
                     onChange={this.handleInputChange}
                   />
+                  <div className="invalid-feedback">{errors.firstname}</div>
                 </div>
               </div>
-              <div className="form-group row my-2">
+              <div className="form-group row my-4">
                 <div className="col-2">
                   <label htmlFor="lastname">Last Name</label>
                 </div>
                 <div className="col-10">
                   <input
                     type="text"
-                    className="form-control"
+                    className={`form-control ${class_lastname} `}
                     name="lastname"
                     id="lastname"
                     placeholder="Last Name"
                     value={this.state.lastname}
+                    onBlur={this.handleBlur("lastname")}
                     onChange={this.handleInputChange}
                   />
+                  <div className="invalid-feedback">{errors.lastname}</div>
                 </div>
               </div>
-              <div className="form-group row my-2">
+              <div className="form-group row my-4">
                 <div className="col-2">
                   <label htmlFor="telnum">Contact Tel.</label>
                 </div>
                 <div className="col-10">
                   <input
                     type="tel"
-                    className="form-control"
+                    className={`form-control ${class_telnum} `}
                     name="telnum"
                     id="telnum"
                     placeholder="Tel. number"
                     value={this.state.telnum}
+                    onBlur={this.handleBlur("telnum")}
                     onChange={this.handleInputChange}
                   />
+                  <div className="invalid-feedback">{errors.telnum}</div>
                 </div>
               </div>
-              <div className="form-group row my-2">
+              <div className="form-group row my-4">
                 <div className="col-2">
                   <label htmlFor="email">Email</label>
                 </div>
                 <div className="col-10">
                   <input
                     type="email"
-                    className="form-control"
+                    className={`form-control ${class_email} `}
                     name="email"
                     id="email"
                     placeholder="Email"
                     value={this.state.email}
+                    onBlur={this.handleBlur("email")}
                     onChange={this.handleInputChange}
                   />
+                  <div className="invalid-feedback">{errors.email}</div>
                 </div>
               </div>
-              <div className="form-group row my-2">
+              <div className="form-group row my-4">
                 <div className="col-6 offset-2">
                   <div className="form-check">
                     <div className="form-check-label">
                       <input
                         type="checkbox"
                         name="agree"
+                        id="agree"
                         className="form-check-input"
                         checked={this.state.agree}
                         onChange={this.handleInputChange}
                       />
-                      <strong>May we contact you ? </strong>
+                      <strong>
+                        <label htmlFor="agree">May we contact you ? </label>
+                      </strong>
                     </div>
                   </div>
                 </div>

2357a7607a21fe2133115a1fdbb77913b5dfc45f
mr.Rhenium <mr.rhenium75614@gmail.com>
2021-11-07 15:09:25 +0530

    React Redux Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,124 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Control, LocalForm } from "react-redux-form";
 
 class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      firstname: "",
-      lastname: "",
-      telnum: "",
-      email: "",
-      agree: false,
-      contactType: "tel",
-      message: "",
-      touched: {
-        firstname: false,
-        lastname: false,
-        telnum: false,
-        email: false,
-      },
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(event) {
-    console.log("Current State is " + JSON.stringify(this.state));
-    alert("Current State is " + JSON.stringify(this.state));
-    event.preventDefault();
-  }
-
-  handleBlur = (field) => (evt) => {
-    this.setState({
-      touched: {
-        ...this.state.touched,
-        [field]: true,
-      },
-    });
-  };
-
-  validate(firstname, lastname, telnum, email) {
-    const errors = {
-      firstname: "",
-      lastname: "",
-      telnum: "",
-      email: "",
-    };
-
-    if (this.state.touched.firstname && firstname.length < 3) {
-      errors.firstname = "First Name should be >= 3 characters";
-    } else if (this.state.touched.firstname && firstname.length > 10) {
-      errors.firstname = "First Name should be <=10 characters";
-    }
-
-    if (this.state.touched.lastname && lastname.length < 3) {
-      errors.lastname = "Last Name should be >= 3 characters";
-    } else if (this.state.touched.lastname && lastname.length > 10) {
-      errors.lastname = "Last Name should be <=10 characters";
-    }
-
-    let reg = /^\d+$/;
-    if (this.state.touched.telnum && !reg.test(telnum)) {
-      errors.telnum = "Tel. Number should only contain Number";
-    }
-
-    if (
-      this.state.touched.email &&
-      email.split("").filter((x) => x === "@").length !== 1
-    ) {
-      errors.email = "Email should contain a @ ";
-    }
-
-    return errors;
+  handleSubmit(values) {
+    console.log("Current State is " + JSON.stringify(values));
+    alert("Current State is " + JSON.stringify(values));
+    // event.preventDefault();
   }
 
   render() {
-    const errors = this.validate(
-      this.state.firstname,
-      this.state.lastname,
-      this.state.telnum,
-      this.state.email
-    );
-
-    // custoum function is start
-
-    let class_firstname;
-    if (this.state.touched.firstname) {
-      if (errors.firstname !== "") class_firstname = "is-invalid";
-      else class_firstname = "is-valid";
-    }
-
-    let class_lastname;
-    if (this.state.touched.lastname) {
-      if (errors.lastname !== "") class_lastname = "is-invalid";
-      else class_lastname = "is-valid";
-    }
-
-    let class_telnum;
-    if (this.state.touched.telnum) {
-      if (errors.telnum !== "") class_telnum = "is-invalid";
-      else class_telnum = "is-valid";
-    }
-
-    let class_email;
-    if (this.state.touched.email) {
-      if (errors.email !== "") class_email = "is-invalid";
-      else class_email = "is-valid";
-    }
-
-    // custom function is over
-
     return (
       <div className="container">
         <div className="row">
@@ -192,24 +88,19 @@ class Contact extends Component {
             <h3>Send us your Feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <form action="" onSubmit={this.handleSubmit}>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
               <div className="form-group row my-4">
                 <div className="col-2">
                   <label htmlFor="firstname">First Name</label>
                 </div>
                 <div className="col-10">
-                  <input
-                    type="text"
-                    className={`form-control ${class_firstname} `}
+                  <Control.text
+                    model=".firstname"
+                    className="form-control"
                     name="firstname"
                     id="firstname"
                     placeholder="First Name"
-                    value={this.state.firstname}
-                    onBlur={this.handleBlur("firstname")}
-                    onChange={this.handleInputChange}
-                    required
                   />
-                  <div className="invalid-feedback">{errors.firstname}</div>
                 </div>
               </div>
               <div className="form-group row my-4">
@@ -217,18 +108,13 @@ class Contact extends Component {
                   <label htmlFor="lastname">Last Name</label>
                 </div>
                 <div className="col-10">
-                  <input
-                    type="text"
-                    className={`form-control ${class_lastname} `}
+                  <Control.text
+                    model=".lastname"
+                    className="form-control"
                     name="lastname"
                     id="lastname"
                     placeholder="Last Name"
-                    value={this.state.lastname}
-                    onBlur={this.handleBlur("lastname")}
-                    onChange={this.handleInputChange}
-                    required
                   />
-                  <div className="invalid-feedback">{errors.lastname}</div>
                 </div>
               </div>
               <div className="form-group row my-4">
@@ -236,18 +122,13 @@ class Contact extends Component {
                   <label htmlFor="telnum">Contact Tel.</label>
                 </div>
                 <div className="col-10">
-                  <input
-                    type="tel"
-                    className={`form-control ${class_telnum} `}
+                  <Control.text
+                    model=".telnum"
+                    className="form-control"
                     name="telnum"
                     id="telnum"
                     placeholder="Tel. number"
-                    value={this.state.telnum}
-                    onBlur={this.handleBlur("telnum")}
-                    onChange={this.handleInputChange}
-                    required
                   />
-                  <div className="invalid-feedback">{errors.telnum}</div>
                 </div>
               </div>
               <div className="form-group row my-4">
@@ -255,32 +136,24 @@ class Contact extends Component {
                   <label htmlFor="email">Email</label>
                 </div>
                 <div className="col-10">
-                  <input
-                    type="email"
-                    className={`form-control ${class_email} `}
+                  <Control.text
+                    model=".email"
+                    className="form-control"
                     name="email"
                     id="email"
                     placeholder="Email"
-                    value={this.state.email}
-                    onBlur={this.handleBlur("email")}
-                    onChange={this.handleInputChange}
-                    required
                   />
-                  <div className="invalid-feedback">{errors.email}</div>
                 </div>
               </div>
               <div className="form-group row my-4">
                 <div className="col-6 offset-2">
                   <div className="form-check">
                     <div className="form-check-label">
-                      <input
-                        type="checkbox"
+                      <Control.checkbox
+                        model=".agree"
                         name="agree"
                         id="agree"
                         className="form-check-input"
-                        checked={this.state.agree}
-                        onChange={this.handleInputChange}
-                        required
                       />
                       <strong>
                         <label htmlFor="agree">May we contact you ? </label>
@@ -289,14 +162,14 @@ class Contact extends Component {
                   </div>
                 </div>
                 <div className="col-3 offset-1">
-                  <select
+                  <Control.select
+                    model=".contactType"
                     name="contactType"
-                    value={this.state.contactType}
-                    onChange={this.handleInputChange}
+                    className="form-control"
                   >
                     <option>Tel.</option>
                     <option>Email</option>
-                  </select>
+                  </Control.select>
                 </div>
               </div>
               <div className="form-group row my-2">
@@ -304,15 +177,14 @@ class Contact extends Component {
                   <label htmlFor="message">Your Feedback</label>
                 </div>
                 <div className="col-10">
-                  <textarea
+                  <Control.textarea
+                    model=".message"
+                    className="form-control"
                     name="message"
                     id="message"
                     cols="50"
                     rows="5"
-                    value={this.state.message}
-                    onChange={this.handleInputChange}
-                    required
-                  ></textarea>
+                  />
                 </div>
               </div>
               <div className="form-group row my-2">
@@ -320,7 +192,7 @@ class Contact extends Component {
                   <button className="btn btn-primary">Send Feedback</button>
                 </div>
               </div>
-            </form>
+            </LocalForm>
           </div>
         </div>
       </div>
